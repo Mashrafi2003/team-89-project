885c65fc415787f0d1b6a6a3b53f3551fae75994
thekawsarhossain <thekawsarhossain@gmail.com>
2022-01-04 18:18:26 +0600

    online courses component added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,7 +1,11 @@
+import { Box } from "@mui/material";
 import React from "react";
+import OnlineCourse from "../OnlineCourse/OnlineCourse";
 
 const Home = () => {
-  return <div></div>;
+  return <Box>
+    <OnlineCourse />
+  </Box>;
 };
 
 export default Home;
